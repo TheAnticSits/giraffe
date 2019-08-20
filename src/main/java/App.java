3f7b6ea30f550cc3769cce03a9cc20cf51db81d9
Ken Messmer <kenmessmerjr@gmail.com>
2019-08-20 18:34:00 -0500

    Nested loop in giraffe academy.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,13 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String [] args){
+    public static void main(String[] args) {
+
+        for(int i = 1; i < 100; i++){
+            for(int j = 1; j < 7; j++){
+                System.out.println("i= "+ i + ", j= " + j);
+            }
+        }
 
     }
 }
