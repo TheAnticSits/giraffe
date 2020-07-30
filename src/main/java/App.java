fffce48680d9f733c7d4077d4bbc126bc229a3eb
Ken Messmer <kenmessmerjr@gmail.com>
2020-07-29 20:43:38 -0500

    Printing out a system.out.println paragraph.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,10 +5,16 @@ public class App {
     public static void main(String[] args) {
 //        System.out.println("Hello World");
 
-        System.out.println("    /|");
+/*        System.out.println("    /|");
         System.out.println("   / |");
         System.out.println("  /  |");
-        System.out.println(" /___|");
+        System.out.println(" /___|");*/
+
+        System.out.println("There once was a man named George,");
+        System.out.println("he was 70 years old.  He really liked");
+        System.out.println("the name George, but didn't like being 70.");
+        System.out.println("Most of his friends were also 70, and one");
+        System.out.println("of them was also named George.");
 
 }
 
