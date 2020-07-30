2a920c0403b022d00ff19392cf6641c236304e6d
Ken Messmer <kenmessmerjr@gmail.com>
2020-07-29 20:39:01 -0500

    Print Triangle using sytem.out.println

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,12 @@ import p1.MyClass;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+//        System.out.println("Hello World");
+
+        System.out.println("    /|");
+        System.out.println("   / |");
+        System.out.println("  /  |");
+        System.out.println(" /___|");
 
 }
 
