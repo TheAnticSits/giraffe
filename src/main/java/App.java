e1aa9a8571ce53b4ee59f4c5e19e4d552c32636e
Ken Messmer <kenmessmerjr@gmail.com>
2020-08-02 14:18:43 -0500

    Changed variables during cascade.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,10 +11,12 @@ public class App {
         System.out.println(" /___|");*/
 
         String characterName = "Dave";
-        int characterAge = 40;
+        int characterAge = 20;
 
         System.out.println("There once was a man named " + characterName + ",");
         System.out.println("he was " + characterAge +" years old.  He really liked");
+        characterName = "Mike";
+        characterAge = 80;
         System.out.println("the name " + characterName + ", but didn't like being " + characterAge + ".");
         System.out.println("Most of his friends were also " + characterAge + ", and one");
         System.out.println("of them was also named " + characterName + ".");
