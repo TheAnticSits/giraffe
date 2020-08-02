318413efb888e84fd327791580f3a6aef3e03c4f
Ken Messmer <kenmessmerjr@gmail.com>
2020-08-02 14:10:15 -0500

    Updated story

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,11 +10,11 @@ public class App {
         System.out.println("  /  |");
         System.out.println(" /___|");*/
 
-        System.out.println("There once was a man named George,");
-        System.out.println("he was 70 years old.  He really liked");
-        System.out.println("the name George, but didn't like being 70.");
-        System.out.println("Most of his friends were also 70, and one");
-        System.out.println("of them was also named George.");
+        System.out.println("There once was a man named Tom,");
+        System.out.println("he was 40 years old.  He really liked");
+        System.out.println("the name Tom, but didn't like being 40.");
+        System.out.println("Most of his friends were also 40, and one");
+        System.out.println("of them was also named Tom.");
 
 }
 
