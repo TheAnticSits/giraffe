f7dfc76eabcd7f5f708fb483c6027ee5b1bcb0c7
Ken Messmer <kenmessmerjr@gmail.com>
2020-08-13 18:36:37 -0500

    creating a calculator

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,7 +6,13 @@ public class App {
 
     public static void main(String[] args) {
 
-        
+        Scanner keyboardInput = new Scanner(System.in);
+        System.out.println("Enter in the first number: ");
+        double num1 = keyboardInput.nextDouble();
+        System.out.println("Enter in the second number: ");
+        double num2 = keyboardInput.nextDouble();
+        System.out.println(num1+num2);
+
 
 /*        //getting user input tutorial 9
 
