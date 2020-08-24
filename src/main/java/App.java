7793c93182bf0e56c0a6aafb01b11524d397e03d
Ken Messmer <kenmessmerjr@gmail.com>
2020-08-23 22:48:08 -0500

    working on some methods on my own.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,9 +3,24 @@ import p1.MyClass;
 import java.util.Scanner;
 
 public class App {
+    public static void main(String[] args){
 
     //Tutorial 14 Return Statements
-    
+
+
+    //practice with methods
+        System.out.println("Begin");
+        Scanner digit = new Scanner(System.in);
+        Scanner hitEnter = new Scanner(System.in);
+        System.out.println("Enter a word");
+        String word = digit.nextLine();
+        System.out.println(word + " is the word you entered.");
+        System.out.println("Ready to have it broke down?  Press Enter.");
+        String enter = hitEnter.nextLine();
+        System.out.println("Your word is " + word.length() + " characters long.");
+        System.out.println(word.toLowerCase() + " is your word in lowerCase.");
+        System.out.println(word.toUpperCase() + " is your word in uppercase.");
+
 
 }
 
